23c5181575aa8cb67992ea7bc58763c54c3efcf1
Bachue Zhou <bachue.shu@gmail.com>
2011-10-05 00:50:06 +0800

    finish algorithm

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 module Codebreaker
 	describe Game do
-		describe "#start" do
-			let(:output) { double('output').as_null_object }
-			let(:game) { Game.new output }
+		let(:output) { double('output').as_null_object }
+		let(:game) { Game.new output }
 
+		describe "#start" do
 			it "should send a welcome message" do
 				output.should_receive(:puts).with('Welcome to CodeBreaker!')
 				game.start '1234'
@@ -16,6 +16,40 @@ module Codebreaker
 				game.start '1234'
 			end
 		end
-	
+
+		describe "#guess" do
+			context "with no matches" do
+				it "should send a mark with ''" do
+					game.start '1234'
+					output.should_receive(:puts).with('')
+					game.guess '5555'
+				end
+			end
+
+			context "with 1 number match" do
+				it "should send a mark with '-'" do
+					game.start '1234'
+					output.should_receive(:puts).with('-')
+					game.guess '2555'
+				end
+			end
+
+			context "with 1 exact match" do
+				it "should send a mark with '+'" do
+					game.start '1234'
+					output.should_receive(:puts).with('+')
+					game.guess '1555'
+				end
+			end
+			
+			context "with 2 number matches" do
+				it "should send a mark with '--'" do
+					game.start '1234'
+					output.should_receive(:puts).with('--')
+					game.guess '2355'
+				end
+			end
+			
+		end
 	end
 end
